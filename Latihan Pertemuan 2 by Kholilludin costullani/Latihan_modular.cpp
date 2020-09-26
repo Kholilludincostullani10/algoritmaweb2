4b09424d2746683042ef4fb31abbf0bbb7654185
Kholilludincostullani10 <71931508+Kholilludincostullani10@users.noreply.github.com>
2020-09-26 20:55:57 +0700

    Add files via upload

---
+++ Latihan Pertemuan 2 by Kholilludin costullani/Latihan_modular.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+
+{
+	int a, b, c;;
+	cout<<"PROGRAM LATIHAN MODULAR"<<endl;
+	cout<<"KHOLILLUDIN COSTULLANI"<<endl;
+	cout<<"sebelum ditukar"<<endl;
+	cout<<"Masukan Nilai A = ";cin>>a;
+	b = a*a ;
+	cout<<"Hasil Kuadrat = "<<b<<endl;
+	c = a;
+	a = b;
+	b = c;
+
+	cin.get();
+	return 0;;
+	
+}
---
+++ Latihan Pertemuan 2 by Kholilludin costullani/modular_tukar.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+void tukar (int *a, int *b){
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c; 
+}
+int main ()
+{
+	int a, b;
+	cout<<"PROGRAM MODULAR TUKAR"<<endl;
+	cout<<"KHOLILLUDIN COSTULLANI"<<endl;
+	cout<<"sebelum ditukar"<<endl;
+	cout<<"Masukan Nilai A = ";cin>>a;
+	cout<<"Masukan Nilai B = ";cin>>b;
+	
+	tukar (&a,&b);
+
+	cout<<"Sesudah ditukar"<<endl;
+	cout<<"Hasil Nilai A = "<<a<<endl;
+	cout<<"Hasil Nilai B = "<<b<<endl;
+	cin.get();
+	return 0;;
+	
+}
